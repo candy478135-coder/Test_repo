c479480ced3f182f5e079a50670cae77c03f4d87
Candy <candy478135@gmail.com>
2026-02-09 16:58:43 +0630

    Olivia

---
+++ example.py
@@ -0,0 +1,5 @@
+name= input("Press Write Your name:\t")
+i=0
+while (i <= 5):
+    print(f"{i} {name}")
+    i += 1
---
+++ tessa.py
@@ -0,0 +1,6 @@
+# for i in range(1,16,2):
+#     print(f"Student:{i}")
+
+name= input("Press Write Your name:\t")
+for c in range(11):
+    print(f"{c} {name}")
